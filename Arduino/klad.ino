f0e2d76f750bd9ffde6a2a32b3c906901ad5d2bc
Loes Vanmeerbeek <Loes.Vanmeerbeek@UGent.be>
2026-04-26 13:21:57 +0200

    toevoeging aan klad: 'studeren' komt tevoorschijn, effectieve studietijd zichtbaar bij instellen

--- Arduino/klad.ino
+++ Arduino/klad.ino
@@ -1,13 +1,13 @@
 // bibliotheek initialiseren nodig voor het LED-scherm
 #include <LiquidCrystal.h>
 
- // Stel het aantal kolommen en rijen van het LCD-scherm in:
+// Stel het aantal kolommen en rijen van het LCD-scherm in:
 const int rs = 12, e = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
-int AanUit=7; // voor 'A', om backlight aan en uit te zetten
+int AanUit = 7; // voor 'A', om backlight aan en uit te zetten
 int Toestand;
 
-//variabelen nodig voor knoppen
+// variabelen nodig voor knoppen
 int knop1 = 6;
 int knop2 = 8;
 int knop3 = 10;
@@ -16,10 +16,10 @@ int knop4 = 13;
 unsigned long laatsteDruk1;
 int WaardeKnop1;
 int VorigeKnop1;
-int WaardeKnop2; 
+int WaardeKnop2;
 int VorigeKnop2;
 int WaardeKnop3;
-int VorigeKnop3 ;
+int VorigeKnop3;
 int waarde = 0;
 int minuten = 00;
 int seconden = 00;
@@ -30,24 +30,24 @@ int i;
 int j;
 
 // variabelen nodig voor timer
-bool timerLoopt=false;
-unsigned long laatstetijd=0;
+bool timerLoopt = false;
+unsigned long laatstetijd = 0;
 
-//variabelen voor debounce, vloeiende werking
+// variabelen voor debounce, vloeiende werking
 unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay =50;
+unsigned long debounceDelay = 50;
 unsigned long lastDebounceTime4;
 
-//variabelen nodig voor speaker
+// variabelen nodig voor speaker
 int speaker = 9;
 
 // variabelen nodig voor Led's
 int ledPinR = A1;
-int ledPinG =A0;
+int ledPinG = A0;
 unsigned long vorigeTijd;
 
-
-enum LedStates {
+enum LedStates
+{
   LED_UIT_G_LED_UIT_R,
   LED_AAN_G_LED_UIT_R,
   LED_UIT_G_LED_AAN_R
@@ -55,12 +55,24 @@ enum LedStates {
 LedStates vorigeState = LED_UIT_G_LED_UIT_R;
 LedStates ledState = LED_UIT_G_LED_UIT_R; // toestel staat uit
 
-//variabelen nodig voor pauze
-int teller=0;
-int pauze=false;
+// variabelen nodig voor pauze
+int teller = 0;
+int pauze = false;
 
-void setup() {
-//PinModes output of input instellen
+// variabelen nodig voor 'studie' en 'studeren'
+int studie = 0;
+int studeren = 0;
+int hoeveelPauze = 0;
+int hoeveelPauzeMin = 0;
+int hoeveelPauzeSec = 0;
+int langePauze = 0;
+int hoeveelStudieMin = 0;
+int hoeveelStudieSec = 0;
+int totaalPauzeSec = 0;
+
+void setup()
+{
+  // PinModes output of input instellen
   pinMode(knop1, INPUT_PULLUP);
   pinMode(knop2, INPUT_PULLUP);
   pinMode(knop3, INPUT_PULLUP);
@@ -70,40 +82,47 @@ void setup() {
   pinMode(ledPinG, OUTPUT);
   pinMode(speaker, OUTPUT);
   digitalWrite(7, LOW);
-//Initiële waarden goedzetten
-  Toestand=0;
-  VorigeKnop1=HIGH;
-  VorigeKnop2=HIGH;
-  VorigeKnop3=HIGH;
-  VorigeKnop4=HIGH;
-   lcd.begin(16, 2);
+  // Initiële waarden goedzetten
+  Toestand = 0;
+  VorigeKnop1 = HIGH;
+  VorigeKnop2 = HIGH;
+  VorigeKnop3 = HIGH;
+  VorigeKnop4 = HIGH;
+  lcd.begin(16, 2);
   Serial.begin(9600);
   vorigeTijd = millis();
   digitalWrite(ledPinG, LOW);
   digitalWrite(ledPinR, LOW);
 }
 
-void loop() {
-  //knop 1: aan/uit LCD-scherm code
+void loop()
+{
+  // knop 1: aan/uit LCD-scherm code
 
   WaardeKnop1 = digitalRead(knop1);
 
-  if (WaardeKnop1 == LOW and VorigeKnop1 == HIGH) { //nieuwe indrukking
+  if (WaardeKnop1 == LOW and VorigeKnop1 == HIGH)
+  { // nieuwe indrukking
 
-    if (millis() - laatsteDruk1 > 200) { //debounce code
+    if (millis() - laatsteDruk1 > 200)
+    { // debounce code
 
       Toestand = !Toestand; // om aan te duiden of scherm aan of uit moet
       laatsteDruk1 = millis();
 
-      if (Toestand == 1) {
+      if (Toestand == 1)
+      {
         digitalWrite(AanUit, HIGH); // backlight aanzetten
-        lcd.display(); // tekst aanzetten
-        lcd.clear(); // zorgen dat er geen tekst staat
+        lcd.display();              // tekst aanzetten
+        lcd.clear();                // zorgen dat er geen tekst staat
         lcd.print("Tijd instellen");
-      } 
-      else {
-        digitalWrite(AanUit, LOW); //backlight afzetten
-        lcd.noDisplay(); //display uizetten
+        lcd.setCursor(5, 1);
+        lcd.print("studie:");
+      }
+      else
+      {
+        digitalWrite(AanUit, LOW); // backlight afzetten
+        lcd.noDisplay();           // display uizetten
       }
     }
   }
@@ -114,64 +133,78 @@ void loop() {
   int reading_H = digitalRead(knop2);
   int reading_L = digitalRead(knop3);
 
-  if (reading_H != VorigeKnop2) { // knop 2 verandert van staat, ontvangt signaalverandering, pas als bounce lang genoeg gestopt 1 en die 1 signaal ontvangt wordt verder gegaan
-    lastDebounceTime = millis(); 
+  if (reading_H != VorigeKnop2)
+  { // knop 2 verandert van staat, ontvangt signaalverandering, pas als bounce lang genoeg gestopt 1 en die 1 signaal ontvangt wordt verder gegaan
+    lastDebounceTime = millis();
   }
 
-  if (reading_L != VorigeKnop3){ // knop 3 verandert van staat
-    lastDebounceTime= millis();
+  if (reading_L != VorigeKnop3)
+  { // knop 3 verandert van staat
+    lastDebounceTime = millis();
   }
   // voor knop 2: optellen
-  if ((millis()-lastDebounceTime)>debounceDelay) { // debounce, het lukt om status te veranderen, vertraging introduceren zodat bij eenmaal contact de status verandert
-    if (reading_H != WaardeKnop2){ 
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  { // debounce, het lukt om status te veranderen, vertraging introduceren zodat bij eenmaal contact de status verandert
+    if (reading_H != WaardeKnop2)
+    {
       WaardeKnop2 = reading_H; // wordt geregistreerd zonder de bouce, anders zou volgende if meerder keren doorlopen worden
-      if (WaardeKnop2 == LOW){
-        waarde++;// begint bij waarde 0, aan de hand van even of oneven zullen de minuten bepaald worden
-        if (waarde%2==0){ // waarde is even, bv. Waarde=2, is er 2 keer 30 seconden opgetelt dus 1 min
-          minuten= waarde/2;
-          seconden =0;
-          }
-        if (waarde%2==1){ // waarde is oneven, bv 1, dus 1 keer 30 seconden
-          minuten = waarde/2;
-          seconden =30;
-          }
-  
+      if (WaardeKnop2 == LOW)
+      {
+        waarde++; // begint bij waarde 0, aan de hand van even of oneven zullen de minuten bepaald worden
+        if (waarde % 2 == 0)
+        { // waarde is even, bv. Waarde=2, is er 2 keer 30 seconden opgetelt dus 1 min
+          minuten = waarde / 2;
+          seconden = 0;
+        }
+        if (waarde % 2 == 1)
+        { // waarde is oneven, bv 1, dus 1 keer 30 seconden
+          minuten = waarde / 2;
+          seconden = 30;
+        }
       }
     }
   }
   // voor knop 3: aftrekken (zelfde als voorheen)
-  if ((millis()-lastDebounceTime)>debounceDelay) {
-    if (reading_L != WaardeKnop3){
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    if (reading_L != WaardeKnop3)
+    {
       WaardeKnop3 = reading_L;
-      if (WaardeKnop3 == LOW){
+      if (WaardeKnop3 == LOW)
+      {
         waarde--;
-        if (waarde%2==0){
-          minuten= waarde/2;
-          seconden =0;
-          }
-        if (waarde%2==1){
-          minuten = waarde/2;
-          seconden =30;
-          }
+        if (waarde % 2 == 0)
+        {
+          minuten = waarde / 2;
+          seconden = 0;
+        }
+        if (waarde % 2 == 1)
+        {
+          minuten = waarde / 2;
+          seconden = 30;
+        }
       }
     }
   }
   VorigeKnop2 = reading_H; // om signaalverandering van signaal op te vangen
   VorigeKnop3 = reading_L;
 
-  //knop 4: tijd start met lopen
+  // knop 4: tijd start met lopen
 
   VorigeKnop2 = reading_H;
   VorigeKnop3 = reading_L;
 
   WaardeKnop4 = digitalRead(knop4);
 
-  if (WaardeKnop4 == LOW and VorigeKnop4 == HIGH) {
-    if (millis() - lastDebounceTime4 > debounceDelay) { // zelfde als voorheen: debounce
-      timerLoopt = !timerLoopt; // verandering in of timer moet aflopen of stoppen met aflopen wanneer knop ingeduwd wordt
+  if (WaardeKnop4 == LOW and VorigeKnop4 == HIGH)
+  {
+    if (millis() - lastDebounceTime4 > debounceDelay)
+    {                               // zelfde als voorheen: debounce
+      timerLoopt = !timerLoopt;     // verandering in of timer moet aflopen of stoppen met aflopen wanneer knop ingeduwd wordt
       lastDebounceTime4 = millis(); // laatste indrukking knop
 
-      if (timerLoopt) {// als de timer loop zijn de beginwaarden..
+      if (timerLoopt)
+      { // als de timer loop zijn de beginwaarden..
         i = minuten;
         j = seconden;
         laatstetijd = millis();
@@ -182,34 +215,40 @@ void loop() {
 
   // aftellende timer
 
-  if (timerLoopt) {
-    if (millis() - laatstetijd >= 1000) { // nadat 1 seconde verlopen is mag de seconde-waarde aangepast worden
+  if (timerLoopt)
+  {
+    if (millis() - laatstetijd >= 1000)
+    { // nadat 1 seconde verlopen is mag de seconde-waarde aangepast worden
       laatstetijd = millis();
       j--;
 
-      if (j < 0) { // als j negatief wordt verminderen de min, en start een nieuwe minuut
+      if (j < 0)
+      { // als j negatief wordt verminderen de min, en start een nieuwe minuut
         j = 59;
         i--;
       }
 
-      if (j > 0 && j < 6 or j > 30 && j < 36){ // j moet gedurende deze intervallen pauze aangeven
-        if (j == 5 or j == 35){ // op de led wisseling zal er een beep geluid te horen zijn
+      if (j > 0 && j < 6 or j > 30 && j < 36)
+      { // j moet gedurende deze intervallen pauze aangeven
+        if (j == 5 or j == 35)
+        { // op de led wisseling zal er een beep geluid te horen zijn
           tone(speaker, 262, 100);
-          teller++; 
+          teller++;
         }
-        //teller++;
         pauze = true; // zal verderop ervoor zorgen dat het systeem zich in de state 'LED_UIT_G_LED_AAN_R' verkeert
       }
-      if (j > 5 && j < 30 or j > 35 && j <= 59) { // j moet gedurende deze intervallen studeren aangeven
+      if (j > 5 && j < 30 or j > 35 && j <= 59)
+      {                // j moet gedurende deze intervallen studeren aangeven
         pauze = false; // zal verderop ervoor zorgen dat het systeem zich in de state 'LED_AAN_G_LED_UIT_R' verkeert
       }
 
-      if (i < 0) { // als i vervolgens negatief wordt, stopt de timer
+      if (i < 0)
+      { // als i vervolgens negatief wordt, stopt de timer
         i = 0;
         j = 0;
         timerLoopt = false;
-        //teller++; // zal pauzeduur bepalen, na het stoppen van de timer is er een cyclus doorlopen
-        pauze=false; // zal verderop ervoor zorgen dat het systeem gedurende een tijd niets meer doet, deze variabele zorgt ervoor dat het systeem voor de eerste cyclus ook werkt
+        // teller++; // zal pauzeduur bepalen, na het stoppen van de timer is er een cyclus doorlopen
+        pauze = false;           // zal verderop ervoor zorgen dat het systeem gedurende een tijd niets meer doet, deze variabele zorgt ervoor dat het systeem voor de eerste cyclus ook werkt
         tone(speaker, 262, 100); // wanneer de timer gedaan is weerklinkt een geluid om de gebruiker te alarmeren
       }
     }
@@ -218,85 +257,145 @@ void loop() {
 
   lcd.setCursor(0, 1); // juiste plaats zetten waar tekst geprint zal worden
 
-  if (digitalRead(AanUit) == HIGH) { // het backlight moet aanstaan, er moet dus op de aan/uit knop geduwd zijn om iets te printen op het scherm
+  if (digitalRead(AanUit) == HIGH)
+  { // het backlight moet aanstaan, er moet dus op de aan/uit knop geduwd zijn om iets te printen op het scherm
 
-    if (timerLoopt) { // als de timer loopt , wordt gewoon telkens de geüpdate tijd geprint
+    if (timerLoopt)
+    { // als de timer loopt , wordt gewoon telkens de geüpdate tijd geprint
       lcd.print(i);
       lcd.print(":");
-      if (j < 10) lcd.print("0"); // ervoor zorgen dat de minuten telkens met 2 cijfers worden weergegeven
-      lcd.print(j); 
-    } else { // als de timer niet loopt, moeten gewoon de ingegeven waarden door knop2 en 3 weergegeven worden
+      if (j < 10)
+        lcd.print("0"); // ervoor zorgen dat de minuten telkens met 2 cijfers worden weergegeven
+      lcd.print(j);
+    }
+    else
+    { // als de timer niet loopt, moeten gewoon de ingegeven waarden door knop2 en 3 weergegeven worden
       lcd.print(minuten);
       lcd.print(":");
-      if (seconden < 10) lcd.print("0");
+      if (seconden < 10)
+        lcd.print("0");
       lcd.print(seconden);
     }
-  } 
-
-  else { // als het scherm niet aanstaat mag er ook geen tekst weergegeven worden
-    lcd.clear(); 
   }
 
-  //wanneer elke led zal branden of net niet
+  else
+  { // als het scherm niet aanstaat mag er ook geen tekst weergegeven worden
+    lcd.clear();
+  }
 
-  //if(digitalRead(AanUit)==LOW){ // als het scherm niet aanstaat, mogen de led's ook nog niet branden -> niet nodig want zit in de else
-      //ledState=LED_UIT_G_LED_UIT_R;
+  // wanneer elke led zal branden of net niet
+
+  // if(digitalRead(AanUit)==LOW){ // als het scherm niet aanstaat, mogen de led's ook nog niet branden -> niet nodig want zit in de else
+  // ledState=LED_UIT_G_LED_UIT_R;
   //}
-  if (timerLoopt){ // als de timer loopt en het scherm staat aan, dan is de groene led aan
-      if (pauze == true){
-        ledState = LED_UIT_G_LED_AAN_R;
-      }
-      else {
-        ledState=LED_AAN_G_LED_UIT_R;
-      }
+  if (timerLoopt)
+  { // als de timer loopt en het scherm staat aan, dan is de groene led aan
+    if (pauze == true)
+    {
+      ledState = LED_UIT_G_LED_AAN_R;
+    }
+    else
+    {
+      ledState = LED_AAN_G_LED_UIT_R;
+    }
   }
-  else { // als het scherm aanstaat en de timer niet loopt
-      ledState=LED_UIT_G_LED_UIT_R;
+  else
+  { // als het scherm aanstaat en de timer niet loopt
+    ledState = LED_UIT_G_LED_UIT_R;
   }
 
   // Leds laten branden, bij variabelen de juiste led's laten branden
-  switch (ledState){
-    case LED_AAN_G_LED_UIT_R: //toestel uit -> studeertijd
-      lcd.setCursor(0,0);
-      lcd.print("                ");
-      //lcd.setCursor(0,0);
-      //lcd.print("studeren");
-      digitalWrite(ledPinG, HIGH);
-      digitalWrite(ledPinR, LOW);
-      lcd.setCursor(11, 1); // juiste plaats zetten waar 'pauze' verwijderd moet worden
-      lcd.print("     "); // 'pauze' verwijderen
-      break;
+  switch (ledState)
+  {
+  case LED_AAN_G_LED_UIT_R: // toestel uit -> studeertijd
+    studie = 0;
+    lcd.setCursor(0, 0);
+    if (studeren == 0)
+    { // variabele werd ingevoerd om zodat de text studeren niet knippert
+      lcd.setCursor(0, 0);
+      lcd.print("                  ");
+      studeren++;
+    }
+    lcd.setCursor(0, 0);
+    lcd.print("studeren");
+    digitalWrite(ledPinG, HIGH);
+    digitalWrite(ledPinR, LOW);
+    lcd.setCursor(5, 1);       // juiste plaats zetten waar 'pauze' verwijderd moet worden
+    lcd.print("            "); // 'pauze' verwijderen
+    break;
 
-    case LED_UIT_G_LED_AAN_R: // studeertijd -> pauze
-      digitalWrite(ledPinG, LOW);
-      digitalWrite(ledPinR, HIGH);
-      lcd.setCursor(11, 1); // juiste plaats zetten waar 'pauze' geprint zal worden
-      lcd.print("pauze"); // 'pauze' komt naast timer
-      if (teller == 4){
-        lcd.setCursor(11,1);
-        lcd.print("     ");
-        lcd.setCursor(0,1);
-        lcd.print("lange pauze");
-        delay(10000);
-        teller = 0;
-        lcd.setCursor(0,1);
-        lcd.print("           ");
+  case LED_UIT_G_LED_AAN_R: // studeertijd -> pauze
+    studeren = 0;
+    digitalWrite(ledPinG, LOW);
+    digitalWrite(ledPinR, HIGH);
+    lcd.setCursor(0, 0);
+    lcd.print("                      ");
+    lcd.setCursor(11, 1); // juiste plaats zetten waar 'pauze' geprint zal worden
+    lcd.print("pauze");   // 'pauze' komt naast timer
+    if (teller == 4)
+    { // voor lange pauzes
+      lcd.setCursor(11, 1);
+      lcd.print("           ");
+      lcd.setCursor(0, 1);
+      lcd.print("lange pauze");
+      delay(10000);
+      teller = 0;
+      lcd.setCursor(0, 1);
+      lcd.print("           ");
+      j = j - 10; // tijd loop door doorheen de pauze
+      if (j < 0)
+      {
+        i--;
+        j = 60 + j;
       }
-      break;
+    }
+    break;
 
-    case LED_UIT_G_LED_UIT_R:
-      digitalWrite(ledPinG, LOW);
-      digitalWrite(ledPinR, LOW);
-      if (digitalRead(AanUit) != LOW) {
-        lcd.setCursor(0,0);
-        lcd.print("Tijd instellen");
+  case LED_UIT_G_LED_UIT_R:
+    digitalWrite(ledPinG, LOW);
+    digitalWrite(ledPinR, LOW);
+    if (digitalRead(AanUit) != LOW)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("Tijd instellen");
+      if (studie == 0)
+      {                           // variabele wordt ingevoerd zodat studie en diens tijd niet flikkert
+        lcd.setCursor(11, 1);     // juiste plaats zetten waar 'pauze' verwijderd moet worden
+        lcd.print("           "); // 'pauze' verwijderen
+        studie++;
       }
-      lcd.setCursor(11, 1); // juiste plaats zetten waar 'pauze' verwijderd moet worden
-      lcd.print("     "); // 'pauze' verwijderen
-      break;
-  }  
-  //speaker toevoegen: speaker laten werken wanneer de status van de Leds verandert
-  if (ledState != vorigeState) {
+      lcd.setCursor(5, 1);
+      lcd.print("studie:");
+      hoeveelPauze = minuten * 2 + seconden / 30; // eerst de hoeveelheid definiëren dat de gebruiker pauze heeft
+      if (hoeveelPauze > 4)
+      { // na 4 keer pauze volgt een lange pauze, berekenen hoeveel deze voorkomt
+        langePauze = hoeveelPauze / 5;
+      }
+      hoeveelPauze = (hoeveelPauze + langePauze) * 5;
+      langePauze = 0;                                // terug resetten, anders blijft tijd bijkomen
+      int totaalStudieSec = minuten * 60 + seconden; // gehele tijd omzetten naar seconden
+      int totaalPauzeSec = hoeveelPauze;
+
+      int resultaatSec = totaalStudieSec - totaalPauzeSec; // hoeveel seconden effectieve studietijd
+
+      hoeveelStudieMin = resultaatSec / 60; // terug naar min + sec
+      hoeveelStudieSec = resultaatSec % 60;
+
+      lcd.print(hoeveelStudieMin); // uitprinten
+      lcd.print(":");
+
+      if (hoeveelStudieSec < 10)
+      {
+        lcd.print("0"); // voor mooie tijden
+      }
+
+      lcd.print(hoeveelStudieSec);
+    }
+    break;
+  }
+  // speaker toevoegen: speaker laten werken wanneer de status van de Leds verandert
+  if (ledState != vorigeState)
+  {
     tone(speaker, 262, 100);
     vorigeState = ledState;
   }
